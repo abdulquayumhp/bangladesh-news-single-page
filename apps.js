60222b7a8ddbbf20a5426faf819be244e2f6bd31
abdulquayumhp <badulquayumhp998@gmailc.com>
2022-09-04 00:44:43 +0600

    code organize

--- apps.js
+++ apps.js
@@ -6,9 +6,16 @@
     // menu category data api 
 const newsPortalData = async()=>{
    
+   try{
     const res = await fetch("https://openapi.programming-hero.com/api/news/categories")
     const data = await res.json()
     return data.data.news_category;
+   }catch(error){
+    console.log(error)
+
+   }
+    
+   
 }
 
     // dynamic menu cetegory fuction 
@@ -44,9 +51,13 @@ const dynamicCart = (id)=>{
     // fuction sniper id 
     const sniper = document.getElementById("sniper").style.display="block"
 
-    fetch(`https://openapi.programming-hero.com/api/news/category/0${id}`)
-    .then (res => res.json())
-    .then (data => displyAllData(data.data)) 
+    try{
+        fetch(`https://openapi.programming-hero.com/api/news/category/0${id}`)
+        .then (res => res.json())
+        .then (data => displyAllData(data.data)) 
+    }catch(error){
+        console.log(error)
+    }
 }
 
 
@@ -86,48 +97,57 @@ const displyAllData=(cards)=>{
 
         // card 
         div.innerHTML=`
-        <div
-          
-            class="card card-side bg-base-100 shadow-xl w-9/12 mx-auto mb-20 md:flex-nowrap flex-wrap"
-          >
-          
-            <img src="${image_url}" class=" lg:w-1/4 md:w-2/4  object-cover" alt="Movie" />
-            
-           
-            <div class="card-body py-10" >
-              <h2 class="card-title">${title}</h2>
-              <p class="py-5">${details.length > 100 ? details.slice(0,100) + "..." : details}</p>
-              <div class="flex justify-between      content-center ">
-                    <div class="flex content-center justify-center ">
+        
+                <div
+                class="card card-side bg-base-100 shadow-xl w-9/12 mx-auto mb-20 md:flex-nowrap flex-wrap"
+            >
+                <img
+                src="${image_url}"
+                class="lg:w-1/4 md:w-2/4 object-cover"
+                alt="Movie"
+                />
+
+                <div class="card-body py-10">
+                <h2 class="card-title">${title}</h2>
+                <p class="py-5">
+                    ${details.length > 100 ? details.slice(0,100) + "..." : details}
+                </p>
+                <div class="flex justify-between content-center flex-wrap">
                     <div class="flex content-center justify-center">
-                        <img src="${img}" alt="" class="w-10 h-10 rounded-full mt-2 mr-5">
+                    <div class="flex content-center justify-center">
+                        <img
+                        src="${img}"
+                        alt=""
+                        class="w-10 h-10 rounded-full mt-2 mr-5"
+                        />
                     </div>
                     <div>
                         <h1 class="font-medium">${name ? name : "N/A"}</h1>
-                        <h1 class="font-medium">${published_date}</h1>
+                        <h1 class="font-medium">
+                        ${published_date ? published_date : "N/A"}
+                        </h1>
                     </div>
                     </div>
                     <div>
                     <div class="pt-3">
                         <i class="fa-regular fa-eye"></i>
-                        <span>${total_view}</span>
+                        <span>${total_view ? total_view : "N/A"}</span>
                     </div>
                     </div>
                     <div>
                     <p class="pt-2">${number} ${badge}</p>
-                
                     </div>
                     <div class="pt-3">
-
-
-                    <label onclick="modalDynamicDataLoad('${_id}')" for="my-modal-3" class="cursor-pointer bg-neutral-100 py-2 px-4"><i class="fa-solid fa-arrow-right"></i></label>
-
+                    <label
+                        onclick="modalDynamicDataLoad('${_id}')"
+                        for="my-modal-3"
+                        class="cursor-pointer bg-neutral-100 py-2 px-4"
+                        ><i class="fa-solid fa-arrow-right"></i
+                    ></label>
                     </div>
-                    </div>
-             
-              </div>
+                </div>
+                </div>
             </div>
-          </div>
         
         `;
         displayCard.appendChild(div)
@@ -149,9 +169,13 @@ displayNewsWebsite()
     // details section 
 const modalDynamicDataLoad=(modalid)=>{
 
-    fetch(`https://openapi.programming-hero.com/api/news/${modalid}`)
-    .then (res => res.json())
-    .then (data => displayModalData(data.data[0])) 
+    try{
+        fetch(`https://openapi.programming-hero.com/api/news/${modalid}`)
+        .then (res => res.json())
+        .then (data => displayModalData(data.data[0]))
+    }catch(error){
+        console.log(error)
+    }
    
 
 }
